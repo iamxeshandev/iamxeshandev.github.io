6ba37e618ac6f84a16d72e2262f930bcbc7907fe
Zeshan Mehmood <iamxeshan@gmail.com>
2025-12-30 02:59:35 +0500

    chore: remove README.md and update next.config.ts to remove basePath for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/iamxeshan-portfolio', // Required for GitHub Pages project sites
   images: {
-    unoptimized: true, // Required for static export
+    unoptimized: true,
   },
 };
 
